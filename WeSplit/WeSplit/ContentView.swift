4c92a03500a5351daa4b257be809826c9d3c48b6
Suryachappidi <58464605+Suryachappidi@users.noreply.github.com>
2020-07-05 00:25:37 +0530

    Add files via upload

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,67 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Surya Chappidi on 04/07/20.
+//  Copyright © 2020 Surya Chappidi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10,15,20,25,0]
+    
+    var totalPerPerson:Double{
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    var body: some View {
+        NavigationView{
+        Form {
+            Section{
+                TextField("Amount",text: $checkAmount)
+                    .keyboardType(.decimalPad)
+                
+                Picker("Number of People" , selection: $numberOfPeople){
+                    ForEach(2 ..< 20) {
+                        Text("\($0)")
+                    }
+                }
+            }
+            Section(header:Text("How much tip do you want to leave?")){
+                
+                Picker("TipPercentage",selection: $tipPercentage){
+                    ForEach(0 ..< tipPercentages.count){
+                        Text("\(self.tipPercentages[$0])%")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            Section{
+                Text("$\(totalPerPerson,specifier: "%0.2f")")
+            }
+        }.navigationBarTitle("WeSplit")
+        }
+        
+    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
